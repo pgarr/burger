ecb0253d07b34f259dcf7539635ba55ac82eaeca
pgarr <garlej.p@gmail.com>
2020-06-01 21:05:00 +0200

    Custom Buttons for order summary; navigation bar init

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Aux from '../../hoc/Auxillary';
 import styles from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const Layout = (props) => (
-  <Aux>
-    <div>Toolbar, SideDrawer, Backrop</div>
+  <React.Fragment>
+    <Toolbar />
     <main className={styles.Content}>{props.children}</main>
-  </Aux>
+  </React.Fragment>
 );
 
 export default Layout;
