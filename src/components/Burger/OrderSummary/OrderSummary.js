5d45e572e44bbfa7ebf109f6112e802291b35d92
pgarr <garlej.p@gmail.com>
2020-06-02 18:58:42 +0200

    Modal shouldUpdate

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,34 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
-    return (
-      <li key={igKey}>
-        <span style={{ textTransform: 'capitalize' }}>{igKey}</span>:{' '}
-        {props.ingredients[igKey]}
-      </li>
+class OrderSummary extends Component {
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(
+      (igKey) => {
+        return (
+          <li key={igKey}>
+            <span style={{ textTransform: 'capitalize' }}>{igKey}</span>:{' '}
+            {this.props.ingredients[igKey]}
+          </li>
+        );
+      }
     );
-  });
 
-  return (
-    <React.Fragment>
-      <h3>Your Order</h3>
-      <p>A delicious with the following ingredients:</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        <strong>Total Price: {props.price.toFixed(2)}$</strong>
-      </p>
-      <p>Continue to Checkout?</p>
-      <Button btnType='Danger' clicked={props.purchaseCancelled}>
-        CANCEL
-      </Button>
-      <Button btnType='Success' clicked={props.purchaseContinued}>
-        CONTINUE
-      </Button>
-    </React.Fragment>
-  );
-};
+    return (
+      <React.Fragment>
+        <h3>Your Order</h3>
+        <p>A delicious with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total Price: {this.props.price.toFixed(2)}$</strong>
+        </p>
+        <p>Continue to Checkout?</p>
+        <Button btnType='Danger' clicked={this.props.purchaseCancelled}>
+          CANCEL
+        </Button>
+        <Button btnType='Success' clicked={this.props.purchaseContinued}>
+          CONTINUE
+        </Button>
+      </React.Fragment>
+    );
+  }
+}
 
-export default orderSummary;
+export default OrderSummary;
