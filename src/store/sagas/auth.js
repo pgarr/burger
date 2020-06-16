4e33f48584e53c902194d267d83423be80f51ef0
pgarr <garlej.p@gmail.com>
2020-06-16 19:39:54 +0200

    saga call, all, takeLatest

--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -1,12 +1,13 @@
-import { delay, put } from 'redux-saga/effects';
+import { delay, put, call } from 'redux-saga/effects';
 import axios from 'axios';
 
 import * as actions from '../actions/index';
 
 export function* logoutSaga(action) {
-  yield localStorage.removeItem('token');
-  yield localStorage.removeItem('expirationDate');
-  yield localStorage.removeItem('userId');
+  // call makes it testable
+  yield call([localStorage, 'removeItem'], 'token');
+  yield call([localStorage, 'removeItem'], 'expirationDate');
+  yield call([localStorage, 'removeItem'], 'userId');
   yield put(actions.logoutSucceed());
 }
 
